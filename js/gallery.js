7b403bf99e2d44346c82acaead1c7d3358ccee77
Fredy Roman Vazquez Geronimo <frvazquez@MacBook-Pro-de-Fredy.local>
2024-04-24 13:08:45 -0600

    Actualizacion de formato

--- js/gallery.js
+++ js/gallery.js
@@ -1,26 +1,26 @@
 /*Name this external file gallery.js*/
 
-function upDate(previewPic){
+function upDate(previewPic) {
         console.log("Evento upDate disparado");
         console.log("Alt: ", previewPic.alt);
         console.log("Fuente: ", previewPic.src);
-        
+
         // Cambiar el texto del elemento con id "image"
         document.getElementById('image').textContent = previewPic.alt;
-        
+
         // Cambiar la imagen de fondo del elemento con id "image"
         document.getElementById('image').style.backgroundImage = "url('" + previewPic.src + "')";
 
 
-       }
-   
-       function unDo(){
+}
+
+function unDo() {
         console.log("Evento unDo disparado");
-    
+
         // Restaurar la imagen de fondo original del elemento con id "image"
         document.getElementById('image').style.backgroundImage = "url('')";
-        
+
         // Restaurar el texto original del elemento con id "image"
         document.getElementById('image').textContent = "Hover over an image below to display here.";
-     
-       }
+
+}
